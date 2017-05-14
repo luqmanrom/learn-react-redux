61149adb5118b007a5502867aa8da1454dba8386
Luqman <luqmanrom@gmail.com>
2017-05-14 18:28:21 +0800

    Boiler plate for react redux

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers'
+
+const App = () => {
+
+	return (
+
+		<Provider store={createStore(reducers)}>
+			<View> 
+				<Text> Hello Redux </Text>
+			</View>
+		</Provider>	
+	);
+};
+
+
+export default App;
