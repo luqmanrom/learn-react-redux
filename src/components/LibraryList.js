7c62e0ad007d21b5b4e74299a90114938848e189
Luqman <luqmanrom@gmail.com>
2017-05-14 19:33:17 +0800

    Wire up state and props

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -5,12 +5,18 @@ import { connect } from 'react-redux';
 class LibraryList extends Component {
 
 	render() {
+
+		console.log(this.props)
 		return;
 	}
 }
 
+// Set the props for the component. Wire up the state and the props
 const mapStateToProps = state => {
-	console.log(state);
+	//console.log(state);
+	return { libraries : state.libraries }; 
 }
+
+
 
 export default connect(mapStateToProps)(LibraryList); // To get access to the state
